4a3c3156cd25873713d26622140e274c6f4839e9
SafiyaElmi <240500598@mycput.ac.za>
2026-03-17 20:43:34 +0200

    Payment class with Factory

--- src/main/java/za/ac/cput/factory/PaymentFactory.java
+++ src/main/java/za/ac/cput/factory/PaymentFactory.java
@@ -1,4 +1,39 @@
 package za.ac.cput.factory;
 
+import za.ac.cput.domain.Payment;
+import za.ac.cput.util.Helper;
+
+/*
+PaymentFactory.java
+Payment factory class
+Author: Safiya Elmi
+(240500598)
+Date: 17 March 2026
+*/
+
 public class PaymentFactory {
+
+    public static Payment createPayment(String paymentRef, double amount, String paymentDate, String paymentMethod,
+                                        String status) {
+
+        if (Helper.isNullOrEmpty(paymentRef)) {
+            throw new IllegalArgumentException("Payment reference cannot be empty");
+        }
+
+        if (Helper.isNullOrEmpty(paymentMethod)) {
+            throw new IllegalArgumentException("Payment method cannot be empty");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
+
+        return new Payment.Builder()
+                .setPaymentRef(paymentRef)
+                .setAmount(String.valueOf(amount))
+                .setPaymentDate(paymentDate)
+                .setPaymentMethod(paymentMethod)
+                .setStatus(status)
+                .build();
+    }
 }
