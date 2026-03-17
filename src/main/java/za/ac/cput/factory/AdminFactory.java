2572732cc9af364220cd5b73a9820230cb455f74
Thimn-a <230232108@mycput.ac.za>
2026-03-17 14:09:58 +0200

    Admin class with Factory

--- src/main/java/za/ac/cput/factory/AdminFactory.java
+++ src/main/java/za/ac/cput/factory/AdminFactory.java
@@ -1,4 +1,17 @@
 package za.ac.cput.factory;
+import za.ac.cput.domain.Admin;
+import za.ac.cput.util.Helper;
+
 
 public class AdminFactory {
+    public static Admin createAdmin (String id, String role) {
+      if (Helper.isNullOrEmpty(id)
+          || Helper.isNullOrEmpty(role)) {
+          return null;
+      }
+      return new Admin.Builder()
+              .setId(id)
+              .setRole(role)
+              .build();
+    }
 }
